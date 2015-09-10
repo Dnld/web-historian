fd45a7b02ef2328346aa2dad9774d63b036fac32
Dnld <steinert.donald@gmail.com>
2015-09-10 13:57:39 -0400

    updates addUrlToList

--- web/public/app.js
+++ web/public/app.js
@@ -5,7 +5,7 @@ $(function() {
     var site = $(this).closest('form').find('.input-field').val();
     console.log('button clicked, value of input:', site);
     $.ajax({
-      url: 'http://27.0.0.1:8080/',
+      url: 'http://127.0.0.1:8080/',
       type: 'POST',
       data: JSON.stringify(site),
       contentType: 'applications/json',
@@ -15,8 +15,8 @@ $(function() {
       error: function(data){
         console.log('error');
       },
-    }); 
-    $('.input-field').empty();
+    });    
+    $('.input-field').val('');
   });
   
 });
